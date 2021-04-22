54994c7bea5486869c430e469eb685bac3c08a92
Jakub <jakub.korsak@protonmail.com>
2021-04-22 19:59:39 +0200

    some basic gate set

--- qc/gates.py
+++ qc/gates.py
@@ -1,5 +1,18 @@
+import qutip.qip.operations.gates as q
 
-class Gates:
+
+class Gate:
+
     gates = {
-
+        'H': q.hadamard_transform,
+        'X': q.sigmax,
+        'Y': q.sigmay,
+        'Z': q.sigmaz,
+        'I': q.qeye
     }
+
+    def __init__(self, key: str) -> None:
+        self.gate = self.gates[key]
+
+    def get(self):
+        return self.gate
