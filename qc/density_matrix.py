d94be8bf5340a583c63b1462a409b03f75e34dd1
Jakub <jakub.korsak@protonmail.com>
2021-04-22 19:03:43 +0200

    get bloch vector out of density matrix

---
+++ qc/density_matrix.py
@@ -0,0 +1,17 @@
+# Density matrix operations
+import numpy as np
+
+
+class DensityMatrix:
+
+    def __init__(self, matrix) -> None:
+        self.state = matrix
+
+    @classmethod
+    def from_diagonals(cls, diagonals: np.array) -> None:
+        cls.state = np.diag(diagonals)
+
+    def get_bloch_vector(self) -> np.array:
+        return np.array([self.state[0, 1] + self.state[1, 0],
+                         0 + (self.state[0, 1] - self.state[1, 0]) * 1j,
+                         self.state[0, 0] - self.state[1, 1]])
