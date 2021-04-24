08e4bdd66eb3b229666a57a9958f113ca03ef3c5
Jakub <jakub.korsak@protonmail.com>
2021-04-24 09:20:39 +0200

    setuptools

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='qc',
+    version='',
+    packages=['qc', 'qc.tests'],
+    url='',
+    license='GPL v3.0',
+    author='Jakub Korsak',
+    author_email='j.korsak3@student.uw.edu.pl',
+    description=''
+)
