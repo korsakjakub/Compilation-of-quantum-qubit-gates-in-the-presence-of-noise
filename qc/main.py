1d1ee666a9857c2ce3b6b53828017f4045bf0652
Jakub Korsak <jakub.korsak@protonmail.com>
2021-05-28 01:07:51 +0200

    moved data functions to data_manager.py, created Program class.

--- qc/main.py
+++ qc/main.py
@@ -1,108 +1,75 @@
 import concurrent.futures
-import csv
-from multiprocessing import Process
+from timeit import default_timer as timer
 
-import matplotlib.pyplot as plt
 import picos
 import picos as pc
-from timeit import default_timer as timer
 from picos import Problem
+from tqdm import tqdm
 
 from qc.bloch_matrix import *
+from qc.data_manager import DataManager, StatesManager
 from qc.word_generator import WordGenerator
 
 
-def lp(min_length: int, max_length: int):
-    rn0 = np.random.default_rng().normal(size=3)
-    n0 = rn0 / np.linalg.norm(rn0)
+class Program:
 
-    output_t = []
-    output_length = []
+    def __init__(self, min_length: int = 3, max_length: int = 4):
+        if max_length > min_length >= 3:
+            self.min_length = min_length
+            self.max_length = max_length
 
-    for length in range(min_length, max_length):
-        wg = WordGenerator(['H', 'T', 'R'], length).generate_words_shorter_than()
-        m = get_bloch_matrices(wg)
-        v = get_bloch_vectors(m)
+    def perform_lp(self):
+        rn0 = np.random.default_rng().normal(size=3)
+        n0 = rn0 / np.linalg.norm(rn0)
 
-        problem = Problem()
-        n = len(v)
-        p = {}
+        output_t = []
+        output_length = []
 
-        # dodaję zmienne
-        for i in range(n - 1):
-            p[i] = picos.RealVariable('p[{0}]'.format(i))
-        t = picos.RealVariable('t')
+        for length in tqdm(range(self.min_length, self.max_length)):
+            sm = StatesManager(bloch=BlochMatrix(), wg=WordGenerator(['H', 'T', 'R'], length))
+            v = sm.get_states()
 
-        # każde p >= 0
-        problem.add_list_of_constraints([p[i] >= 0 for i in range(n - 1)])
-        # p sumują się do 1
-        problem.add_constraint(1 == pc.sum([p[i] for i in range(n - 1)]))
-        # wiąz na wektory
-        problem.add_constraint(t * n0[0] == pc.sum([p[j] * v[j][0] for j in range(n - 1)]))
-        problem.add_constraint(t * n0[1] == pc.sum([p[j] * v[j][1] for j in range(n - 1)]))
-        problem.add_constraint(t * n0[2] == pc.sum([p[j] * v[j][2] for j in range(n - 1)]))
+            problem = Problem()
+            n = len(v)
+            p = {}
 
-        problem.set_objective("max", t)
-        solution = problem.solve(solver='mosek')
-        output_t.append(float(t))
-        output_length.append(length)
-    return [output_length, output_t, n0]
+            # dodaję zmienne
+            for i in range(n):
+                p[i] = picos.RealVariable('p[{0}]'.format(i))
+            t = picos.RealVariable('t')
 
+            # każde p >= 0
+            problem.add_list_of_constraints([p[i] >= 0 for i in range(n)])
+            # p sumują się do 1
+            problem.add_constraint(1 == pc.sum([p[i] for i in range(n)]))
+            # wiąz na wektory
+            problem.add_constraint(t * n0[0] == pc.sum([p[j] * v[j][0] for j in range(n)]))
+            problem.add_constraint(t * n0[1] == pc.sum([p[j] * v[j][1] for j in range(n)]))
+            problem.add_constraint(t * n0[2] == pc.sum([p[j] * v[j][2] for j in range(n)]))
 
-def lp_for_threading(min_length=3, max_length=8, threads=2):
-    results = []
+            problem.set_objective("max", t)
+            problem.solve(solver='mosek')
+            output_t.append(float(t))
+            output_length.append(length)
+        return [output_length, output_t, n0]
 
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        results = [executor.submit(lp, min_length, max_length) for _ in range(threads)]
+    def threaded_lp(self, threads: int = 2):
 
-        for f in concurrent.futures.as_completed(results):
-            results.append(f.result())
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            results = [executor.submit(self.perform_lp) for _ in range(threads)]
 
-    # print(results[2:])
-    return results[threads:]
+            for f in concurrent.futures.as_completed(results):
+                results.append(f.result())
 
-
-def plot_output(ll, t, path: str = "out.png"):
-    plt.plot(ll, t)
-    plt.xlabel('L')
-    plt.ylabel('t')
-    plt.title('t(L)')
-    plt.savefig(path)
-
-
-def write_results(path: str, results: list, open_type: str = "w"):
-    output_file = open(path, open_type)
-    t = []
-    n0 = []
-    ll = []
-    for key in results:
-        ll.append(key[0])
-        t.append(key[1])
-        n0.append(key[2])
-    mean_t = []
-    for el in np.transpose(t):
-        mean_t.append(np.mean(el))
-
-    for i in range(len(mean_t)):
-        output_file.write(str(ll[0][i]) + ", " + str(mean_t[i]) + "\n")
-    output_file.close()
-
-
-def file_to_png(in_path: str = "out.txt", out_path: str = "out.png"):
-    ll = []
-    t = []
-    with open(in_path) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for row in csv_reader:
-            ll.append(int(row[0]))
-            t.append(float(row[1]))
-    plot_output(ll, t, out_path)
+        return results[threads:]
 
 
 if __name__ == "__main__":
     start = timer()
-    res = lp_for_threading(9, 10, 12)
-    write_results("out.txt", res)
-    file_to_png()
+    program = Program(3, 6)
+    writer = DataManager()
+    res = program.threaded_lp(2)
+    writer.write_results(res)
+    writer.file_to_png()
     end = timer()
     print(f'czas: {end - start} s')
