1a051b97095b0f3987d683cdd6d2e65b397f9e58
Jakub Korsak <jakub.korsak@protonmail.com>
2021-06-08 01:22:54 +0200

    made targets generation from outside lp and ALSO working properly. Each thread gets its own target, the same for all lengths.

--- qc/main.py
+++ qc/main.py
@@ -7,14 +7,14 @@ from picos import Problem
 from tqdm import tqdm
 
 from qc.bloch_matrix import *
-from qc.data_manager import DataManager, StatesManager
+from qc.data_manager import DataManager, StatesManager, remove_far_points
 from qc.word_generator import WordGenerator
 
 
 class Program:
 
     def __init__(self, min_length: int = 3, max_length: int = 4):
-        if max_length > min_length >= 3:
+        if max_length > min_length >= 2:
             self.min_length = min_length
             self.max_length = max_length
         else:
@@ -28,8 +28,10 @@ class Program:
 
             problem = Problem()
             index = length - self.min_length
-            n = len(v[index])
             p = {}
+            # take only a specified number of input vectors
+            vec = remove_far_points(v[index].states, target=n0, out_length=500)
+            n = len(vec)
 
             # dodaję zmienne
             for i in range(n):
@@ -41,9 +43,9 @@ class Program:
             # p sumują się do 1
             problem.add_constraint(1 == pc.sum([p[i] for i in range(n)]))
             # wiąz na wektory
-            problem.add_constraint(t * n0[0] == pc.sum([p[j] * v[index][j][0] for j in range(n)]))
-            problem.add_constraint(t * n0[1] == pc.sum([p[j] * v[index][j][1] for j in range(n)]))
-            problem.add_constraint(t * n0[2] == pc.sum([p[j] * v[index][j][2] for j in range(n)]))
+            problem.add_constraint(t * n0[0] == pc.sum([p[j] * vec[j][0] for j in range(n)]))
+            problem.add_constraint(t * n0[1] == pc.sum([p[j] * vec[j][1] for j in range(n)]))
+            problem.add_constraint(t * n0[2] == pc.sum([p[j] * vec[j][2] for j in range(n)]))
 
             problem.set_objective("max", t)
             problem.solve(solver='mosek')
@@ -55,13 +57,19 @@ class Program:
 
         with concurrent.futures.ProcessPoolExecutor() as executor:
             v = []
-            for length in tqdm(range(self.min_length, self.max_length)):
+            n0 = []
+
+            # generate random targets for each thread
+            for _ in range(threads):
                 rn0 = np.random.default_rng().normal(size=3)
-                n0 = rn0 / np.linalg.norm(rn0)
+                n0.append(rn0 / np.linalg.norm(rn0))
+            # for each length generate input vectors - independent of target for now
+            for length in tqdm(range(self.min_length, self.max_length)):
                 wg = WordGenerator(gates, length)
-                sm = StatesManager(bloch=bloch, wg=wg, target=n0)
+                sm = StatesManager(bloch=bloch, wg=wg)
                 v.append(sm.get_states())
-            results = [executor.submit(self.perform_lp, v, n0) for _ in range(threads)]
+
+            results = [executor.submit(self.perform_lp, v, n0[i]) for i in range(threads)]
 
             for f in concurrent.futures.as_completed(results):
                 results.append(f.result())
@@ -69,12 +77,13 @@ class Program:
 
 
 if __name__ == "__main__":
-    start = timer()
     gates = ['H', 'T', 'R', 'X', 'Y', 'Z']
-    program = Program(min_length=8, max_length=10)
-    writer = DataManager()
-    res = program.threaded_lp(gates=gates, bloch=BlochMatrix(visibility=0.95), threads=15)
-    writer.write_results(res)
-    writer.file_to_png()
-    end = timer()
-    print(f'czas: {end - start} s')
+    for _ in range(5):
+        start = timer()
+        program = Program(min_length=2, max_length=9)
+        writer = DataManager()
+        res = program.threaded_lp(gates=gates, bloch=BlochMatrix(visibility=0.95), threads=15)
+        writer.write_results(res)
+        writer.file_to_png()
+        end = timer()
+        print(f'czas: {end - start} s')
