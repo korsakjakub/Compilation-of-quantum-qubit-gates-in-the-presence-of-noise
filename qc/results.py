147c0b8372b71902ecf73acad2ebc6f734521c92
Jakub Korsak <jakub.korsak@protonmail.com>
2022-09-05 18:45:08 +0200

    results

--- qc/results.py
+++ qc/results.py
@@ -54,7 +54,7 @@ class Results(object):
 
         plt.xticks([3, 7, 12])
         ax[0].set_ylabel("Distance to target")
-        plt.legend(loc='upper center', bbox_to_anchor=(-0.5, -0.2),
+        plt.legend(loc='upper center', bbox_to_anchor=(-0.5, -0.17),
                    fancybox=False, shadow=False, ncol=4)
         plt.savefig(self.dir + "dofL.png", dpi=300)
 
@@ -70,17 +70,12 @@ class Results(object):
             noise_param_vs_d4 = []
             path = self.dir + program + "/"
             for file in os.listdir(path):
-                with open(path + file, "r") as f:
-                    noise_param = float(file[3:])
-                    _, t1, t2, t3, d1, d2, d3, d4 = f.read().split("\t")
-#                   d1 = [float(item) for item in d1[1:len(d1)-2].split(", ")][0]
-#                   d2 = [float(item) for item in d2[1:len(d2)-2].split(", ")][0]
-#                   d3 = [float(item) for item in d3[1:len(d3)-2].split(", ")][0]
-#                   d4 = [float(item) for item in d4[1:len(d4)-2].split(", ")][0]
-                    noise_param_vs_d1.append([noise_param, d1])
-                    noise_param_vs_d2.append([noise_param, d2])
-                    noise_param_vs_d3.append([noise_param, d3])
-                    noise_param_vs_d4.append([noise_param, d4])
+                noise_param = float(file[3:])*100
+                d1, d2, d3, d4 = np.genfromtxt(path+file, delimiter="\t").T
+                noise_param_vs_d1.append([noise_param, np.mean(d1)])
+                noise_param_vs_d2.append([noise_param, np.mean(d2)])
+                noise_param_vs_d3.append([noise_param, np.mean(d3)])
+                noise_param_vs_d4.append([noise_param, np.mean(d4)])
             noise_param_vs_d1.sort(key=lambda x: x[0])
             noise_param_vs_d2.sort(key=lambda x: x[0])
             noise_param_vs_d3.sort(key=lambda x: x[0])
@@ -95,32 +90,22 @@ class Results(object):
             ax[i].plot(noise, d2, label="Optimized")
             ax[i].plot(noise, d3, label="Conic")
             ax[i].plot(noise, d4, label="Deterministic")
-            print(f"d1: {d1}\nd2: {d2}\nd3: {d3}\nd4: {d4}")
-
-        #           ax[i].plot(rn, d1, label="Mixing")
-#           ax[i].plot(rn, d2, label="Optimized")
-#           ax[i].plot(rn, d3, label="Conic")
-#           ax[i].plot(rn, d4, label="Deterministic")
-#           ax[i].set_xlabel("Depth of compilation")
-
             ax[i].annotate(labels[i], xy=(0.1, 0.9), xycoords="axes fraction")
-            ax[i].set_xlabel("Noise parameter")
-            ax[i].set_xlim([0.0, 0.04])
-            ax[i].set_ylim([0.0, 0.35])
+            ax[i].set_xlabel("Noise parameter [%]")
+            ax[i].set_xlim([0.0, 0.09])
+            ax[i].set_ylim([0.0, 0.5])
             i += 1
         ax[0].set_ylabel("Distance to target")
-        ax[0].set_xticks(np.arange(0.0, 0.04, 0.01))
-        #plt.xticks(np.arange(0.0, 0.1, 0.02))
-        plt.legend(loc='upper center', bbox_to_anchor=(0, -0.2),
+        #ax[0].set_xticks(np.arange(1, 10, 2))
+        ax[0].set_xticks([1, 3, 5, 7, 9])
+        plt.legend(loc='upper center', bbox_to_anchor=(0, -0.17),
                    fancybox=False, shadow=False, ncol=4)
-        plt.show()
-
-        #plt.savefig(self.dir + "dofeta.png", dpi=300)
+        plt.savefig(self.dir + "dofeta.png", dpi=300)
 
 
 if __name__ == "__main__":
     res = Results()
-    output_paths = ["no-noise-28072022", "depolarizing-28072022", "amplitude-damping-26072022"]
-    #res.plot_depth(["no-noise-13052022", "depolarizing-13052022", "amplitude-damping-13052022"])
-    #  res.plot_noise_param(["depolarizing-eta-13052022", "amplitude-damping-eta-31052022"])#, "amplitude-damping-13052022"])
-    res.plot_depth(output_paths)
+    output_paths_depth = ["no-noise-28072022", "depolarizing-28072022", "amplitude-damping-26072022"]
+    output_paths_noise = ["depolarizing-eta-29072022", "amplitude-damping-eta-26072022"]
+    res.plot_noise_param(output_paths_noise)
+    # res.plot_depth(output_paths_depth)
