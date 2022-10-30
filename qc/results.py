9e0d6f65b86335a53ed52bf56e7c6621f20346a6
Jakub Korsak <jakub.korsak@protonmail.com>
2022-10-30 10:51:57 +0100

    results format change

--- qc/results.py
+++ qc/results.py
@@ -64,63 +64,44 @@ class Results(object):
         labels = ["Depolarizing", "Amplitude damping"]
         i = 0
         for program in programs:
-            noise_param_vs_d1 = []
-            noise_param_vs_d2 = []
-            noise_param_vs_d3 = []
-            noise_param_vs_d4 = []
             path = self.dir + program + "/"
+            dvseta = {}
+            noise_params = []
             for file in os.listdir(path):
                 with open(path + file, "r") as f:
-                    noise_param = float(file[3:])
-                    _, t1, t2, t3, d1, d2, d3, d4 = f.read().split("\t")
-#                   d1 = [float(item) for item in d1[1:len(d1)-2].split(", ")][0]
-#                   d2 = [float(item) for item in d2[1:len(d2)-2].split(", ")][0]
-#                   d3 = [float(item) for item in d3[1:len(d3)-2].split(", ")][0]
-#                   d4 = [float(item) for item in d4[1:len(d4)-2].split(", ")][0]
-                    noise_param_vs_d1.append([noise_param, d1])
-                    noise_param_vs_d2.append([noise_param, d2])
-                    noise_param_vs_d3.append([noise_param, d3])
-                    noise_param_vs_d4.append([noise_param, d4])
-            noise_param_vs_d1.sort(key=lambda x: x[0])
-            noise_param_vs_d2.sort(key=lambda x: x[0])
-            noise_param_vs_d3.sort(key=lambda x: x[0])
-            noise_param_vs_d4.sort(key=lambda x: x[0])
+                    noise_params.append(file.split("V", 1)[1])
+                    d = [np.loadtxt(path + file)[:, i] for i in range(4)]
+                    d = [np.mean(col) for col in d]
+                    dvseta[float(noise_params[-1])] = d
+            pd = np.array([dvseta[i] for i in sorted(dvseta)]).T
+            noise_params = sorted(noise_params)
 
-            noise = np.array(noise_param_vs_d1)[:, :1]
-            d1 = np.array(noise_param_vs_d1)[:, 1:].flatten()
-            d2 = np.array(noise_param_vs_d2)[:, 1:].flatten()
-            d3 = np.array(noise_param_vs_d3)[:, 1:].flatten()
-            d4 = np.array(noise_param_vs_d4)[:, 1:].flatten()
-            ax[i].plot(noise, d1, label="Mixing")
-            ax[i].plot(noise, d2, label="Optimized")
-            ax[i].plot(noise, d3, label="Conic")
-            ax[i].plot(noise, d4, label="Deterministic")
-            print(f"d1: {d1}\nd2: {d2}\nd3: {d3}\nd4: {d4}")
 
-        #           ax[i].plot(rn, d1, label="Mixing")
-#           ax[i].plot(rn, d2, label="Optimized")
-#           ax[i].plot(rn, d3, label="Conic")
-#           ax[i].plot(rn, d4, label="Deterministic")
-#           ax[i].set_xlabel("Depth of compilation")
+            ax[i].plot(noise_params, pd[0], label="Mixing")
+            ax[i].plot(noise_params, pd[1], label="Optimized")
+            ax[i].plot(noise_params, pd[2], label="Conic")
+            ax[i].plot(noise_params, pd[3], label="Deterministic")
 
             ax[i].annotate(labels[i], xy=(0.1, 0.9), xycoords="axes fraction")
             ax[i].set_xlabel("Noise parameter")
-            ax[i].set_xlim([0.0, 0.04])
-            ax[i].set_ylim([0.0, 0.35])
+            #ax[i].set_xlim([0.0, 0.1])
+            #ax[i].set_ylim([0.0, 0.5])
             i += 1
         ax[0].set_ylabel("Distance to target")
-        ax[0].set_xticks(np.arange(0.0, 0.04, 0.01))
+        ax[0].set_xticks(np.arange(0.0, 0.1, 0.01))
         #plt.xticks(np.arange(0.0, 0.1, 0.02))
         plt.legend(loc='upper center', bbox_to_anchor=(0, -0.2),
                    fancybox=False, shadow=False, ncol=4)
-        plt.show()
-
-        #plt.savefig(self.dir + "dofeta.png", dpi=300)
+        #plt.show()
+        plt.savefig(self.dir + "dofeta.png", dpi=300)
 
 
 if __name__ == "__main__":
     res = Results()
-    output_paths = ["no-noise-28072022", "depolarizing-28072022", "amplitude-damping-26072022"]
+    depth = ["no-noise-28072022", "depolarizing-28072022", "amplitude-damping-26072022"]
+    noise = ["depolarizing-eta-29072022", "amplitude-damping-eta-26072022"]
     #res.plot_depth(["no-noise-13052022", "depolarizing-13052022", "amplitude-damping-13052022"])
     #  res.plot_noise_param(["depolarizing-eta-13052022", "amplitude-damping-eta-31052022"])#, "amplitude-damping-13052022"])
-    res.plot_depth(output_paths)
+    #res.plot_depth(depth)
+    res.plot_noise_param(noise)
+
